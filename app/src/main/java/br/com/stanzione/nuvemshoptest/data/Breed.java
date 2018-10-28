268f4e366d866355bf0587e8251da0b9bf9f1fea
Leandro Stanzione <stanzione.leandro@gmail.com>
2018-10-28 14:01:31 -0300

    Add Cat class draft

---
+++ app/src/main/java/br/com/stanzione/nuvemshoptest/data/Breed.java
@@ -0,0 +1,4 @@
+package br.com.stanzione.nuvemshoptest.data;
+
+public class Breed {
+}
---
+++ app/src/main/java/br/com/stanzione/nuvemshoptest/data/Cat.java
@@ -0,0 +1,71 @@
+package br.com.stanzione.nuvemshoptest.data;
+
+import java.util.Date;
+import java.util.List;
+
+public class Cat {
+
+    private String id;
+    private String url;
+    private String subId;
+    private Date createdAt;
+    private String originalFilename;
+    private List<Breed> breedList;
+    private List<Category> categoryList;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getSubId() {
+        return subId;
+    }
+
+    public void setSubId(String subId) {
+        this.subId = subId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getOriginalFilename() {
+        return originalFilename;
+    }
+
+    public void setOriginalFilename(String originalFilename) {
+        this.originalFilename = originalFilename;
+    }
+
+    public List<Breed> getBreedList() {
+        return breedList;
+    }
+
+    public void setBreedList(List<Breed> breedList) {
+        this.breedList = breedList;
+    }
+
+    public List<Category> getCategoryList() {
+        return categoryList;
+    }
+
+    public void setCategoryList(List<Category> categoryList) {
+        this.categoryList = categoryList;
+    }
+}
