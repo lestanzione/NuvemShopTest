a7594cbbb8275a3898836f8dcd2749244e543abf
Leandro Stanzione <stanzione.leandro@gmail.com>
2018-10-28 15:33:54 -0300

    Change API limit and order as ordered in the test

--- app/src/main/java/br/com/stanzione/nuvemshoptest/api/CatApi.java
+++ app/src/main/java/br/com/stanzione/nuvemshoptest/api/CatApi.java
@@ -11,13 +11,13 @@ import retrofit2.http.Query;
 public interface CatApi {
 
     static final String API_KEY = "a24ab4d8-2620-4e21-8fcc-a7e50894a7b9";
-    static final int LIMIT = 20;
+    static final int LIMIT = 25;
 
     @Headers({
             "Content-type: application/json",
             "x-api-key: " + API_KEY
     })
-    @GET("images/search?mime_types=jpg,png&order=ASC&format=json")
+    @GET("images/search?mime_types=jpg,png&order=RANDOM&format=json")
     Observable<List<Cat>> fetchCats(@Query("limit") int limit);
 
 }
